48a04d4493006d490520dcca40f22f5fb14f0b04
KunFang <fangkun930601@me.com>
2024-11-16 21:41:05 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GISTA',
+    version='1.0',
+    packages=find_packages(),  # Automatically find the 'GISTA' package
+    install_requires=[
+        'Click', 'scipy', 'numpy', 'matplotlib', 'pandas', 'seaborn', 'tqdm', 'logomaker'
+    ],
+    entry_points='''
+        [console_scripts]
+        GISTA=GISTA.GISTA:cli
+    '''
+)
