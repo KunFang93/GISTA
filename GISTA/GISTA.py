129a5b913d2143fcedaa938775b40aa58fa80305
KunFang <fangkun930601@me.com>
2024-11-19 22:41:01 -0500

    Add files via upload

--- GISTA/GISTA.py
+++ GISTA/GISTA.py
@@ -24,6 +24,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
 from .GISTA_Util import *
+from natsort import natsort_keygen
 
 # class for setting up subcommand order.
 # Inspired by https://stackoverflow.com/questions/47972638/how-can-i-define-the-order-of-click-sub-commands-in-help
@@ -176,7 +177,6 @@ def multi(samplesfile, comparison, binsize, groupcut, individualcut, outdir):
         features_df_marked['ChangeCodes'] = features_df_marked['tads_id'].map(summarize_dict)
         features_df_marked = pd.merge(features_df_marked, sample_changes, on='tads_id',how='outer')
         features_df_marked = features_df_marked.fillna('NotAvail')
-        # plot sample change logo
         # plot sample change logo
         try:
             ivh_nd_norm = plotlogo(summarize_list_dict['HS']['ND'], '{}/{}_IVH_ND_logo.png'.format(outdir, comp))
@@ -426,6 +426,11 @@ def two(samplesfile, comparison, binsize, groupcut, individualcut, pseudorep, ou
         mask = np.full(len(features_df_marked), False)
         mask[features_df_marked[features_df_marked['GroupChange'] == "SV"].index.values] = True
         features_df_marked['pval_SV'] = np.where(mask, np.minimum(cur_pvals[0], cur_pvals[1]), cur_pvals[1])
+        # sort
+        features_df_marked = features_df_marked.sort_values(
+            by=['chr', 'start(min)', 'end(max)'],
+            key=natsort_keygen()
+        )
         features_df_dchange = features_df_marked.loc[features_df_marked['GroupChange'] == 'SV', :]
         # save as excel
         with pd.ExcelWriter("{}/{}_tads_sub_array_marks.xlsx".format(outdir, comp),
